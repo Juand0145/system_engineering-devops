45f532552a7fa835b77c06cfc8f063e68b2b729b
Juand0145 <juand0145@gmail.com>
2021-08-15 13:13:41 -0500

    Adding task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#client SSH configuration file so
+#that you can connect to a server without typing a password.
+include stdlib
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/holberton',
+  replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
