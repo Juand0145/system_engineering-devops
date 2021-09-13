a7a9784344a8912e708cd7c30624a416134e3f41
Juand0145 <juand0145@gmail.com>
2021-09-13 17:22:47 -0500

    fixing task 8

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
 """Is Python script to export data in the JSON format."""
+"""Exports tasks owned by specified USER_ID in JSON format"""
 import json
-from os import lseek
 import requests
 from sys import argv
 
+if __name__ == "__main__":
 
-if __name__ == '__main__':
     USER_ID = argv[1]
-    json_dict = {}
-
-    request = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                           format(argv[1])).json()
-
-    USERNAME = request.get("username")
-
-    request = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".
-        format(argv[1])).json()
-
-    for i in request:
-        json_dict[USER_ID] = [{"task": i.get("title"),
-                               "completed": i.get("completed"),
-                               "username": USERNAME}]
+    user_dict = {}
+    r = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                     format(argv[1])).json()
+    USERNAME = r.get("username")
+    r = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                     format(argv[1])).json()
+    user_dict[USER_ID] = [{"task": i.get("title"),
+                           "completed": i.get("completed"),
+                           "username": USERNAME} for i in r]
 
     with open(USER_ID + ".json", "w") as jsonfile:
-        json.dump(json_dict, jsonfile)
+        json.dump(user_dict, jsonfile)
