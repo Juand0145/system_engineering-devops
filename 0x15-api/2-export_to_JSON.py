c30a63a2411528848012588c60949d964c4b3954
Juand0145 <juand0145@gmail.com>
2021-09-13 17:25:11 -0500

    fixing pep8

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Is Python script to export data in the JSON format."""
-"""Exports tasks owned by specified USER_ID in JSON format"""
 import json
 import requests
 from sys import argv
@@ -8,15 +7,20 @@ from sys import argv
 if __name__ == "__main__":
 
     USER_ID = argv[1]
-    user_dict = {}
-    r = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                     format(argv[1])).json()
-    USERNAME = r.get("username")
-    r = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                     format(argv[1])).json()
-    user_dict[USER_ID] = [{"task": i.get("title"),
+    json_dict = {}
+
+    request = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                           format(argv[1])).json()
+
+    USERNAME = request.get("username")
+
+    request = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".
+        format(argv[1])).json()
+
+    json_dict[USER_ID] = [{"task": i.get("title"),
                            "completed": i.get("completed"),
-                           "username": USERNAME} for i in r]
+                           "username": USERNAME} for i in request]
 
     with open(USER_ID + ".json", "w") as jsonfile:
-        json.dump(user_dict, jsonfile)
+        json.dump(json_dict, jsonfile)
