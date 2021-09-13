4847b67d567c276594619465a8db9a99c025c79a
Juand0145 <juand0145@gmail.com>
2021-09-13 16:21:13 -0500

    Adding task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Is a Python script that, using this REST API, for a given=employee
+ID, returns information about his/her TODO list progress."""
+
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = []
+
+    request = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                           format(argv[1])).json()
+
+    EMPLOYEE_NAME = request.get('name')
+
+    request = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.
+        format(argv[1])).json
+
+    for i in request:
+        if i.get('competed') is True:
+            TASK_TITLE.append(i.get('title'))
+            NUMBER_OF_DONE_TASKS += 1
+
+    TOTAL_NUMBER_OF_TASKS = len(request)
+
+    print('Employee {} is done with task ({}/{}):'.
+          format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+    for task in TASK_TITLE:
+        print('\t {}'.format(task))
