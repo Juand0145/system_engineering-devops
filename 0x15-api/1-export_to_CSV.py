c7d880e67c1a1a5efef1b3495e40b9cb0465b76c
Juand0145 <juand0145@gmail.com>
2021-09-13 16:56:39 -0500

    Adding task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 """Is a Python script to export data in the CSV format"""
 
-import csv
-import requests
-from sys import argv
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
 
-if __name__ == '__main__':
     USER_ID = argv[1]
+    r = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                     format(USER_ID)).json()
+    USERNAME = r.get("username")
+    r = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                     format(USER_ID)).json()
 
-    request = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                           format(argv[1])).json()
-
-    USERNAME = request.get("name")
-
-    request = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".
-        format(argv[1])).json()
-
-    with open("{}.csv".format(USER_ID), "w") as csv_file:
-        file = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        for i in request:
-            file.writerow(
-                [argv[1], USERNAME, i.get("completed"), i.get("title")])
+    with open(USER_ID + ".csv", "w") as csvfile:
+        f = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for i in r:
+            f.writerow([argv[1], USERNAME, i.get("completed"), i.get("title")])
