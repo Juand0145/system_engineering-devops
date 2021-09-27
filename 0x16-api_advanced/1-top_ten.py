11b61b11b31f2ef76cbfaee83157961b79db3a4a
Juand0145 <juand0145@gmail.com>
2021-09-27 16:31:05 -0500

    Adding task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" How many subs? file"""
+
+
+def top_ten(subreddit):
+    """scrypt to search the number fo 10 hot spots"""
+    import requests
+    import json
+
+    # note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
+    auth = requests.auth.HTTPBasicAuth(
+        'm8o3DcygIBR6fKDWjyNaUA', 'VMkfMswZ-UlQqUjyxYC-ZRPCq45q-g')
+
+    # here we pass our login method (password), username, and password
+    data = {'grant_type': 'password',
+            'username': 'Juand01445',
+            'password': 'Dolex0145'}
+
+    # setup our header info, which gives reddit a brief description of our app
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+
+    # send our request for an OAuth token
+    res = requests.post('https://www.reddit.com/api/v1/access_token',
+                        auth=auth, data=data, headers=headers)
+
+    # convert response to JSON and pull access_token value
+    TOKEN = res.json()['access_token']
+
+    # add authorization to our headers dictionary
+    headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+
+    # while the token is valid (~2 hours) we just add headers=headers to our requests
+
+    try:
+        url = 'https://oauth.reddit.com/r/{:s}/hot'.format(subreddit)
+        byte_string = requests.get(
+            url, headers=headers, params={"limit": 10}).content
+        decoded_string = byte_string.decode("utf8")
+        json_dictionary = json.loads(decoded_string)
+
+        hot_spots = json_dictionary["data"]["children"]
+
+        for element in hot_spots:
+            print(element["data"]["title"])
+
+    except:
+        return (0)
